3f138fddce4ae735396dc6c739445123897d0db5
Luke Triano <luketriano115@gmail.com>
2021-05-16 23:36:05 -0700

    Branding.

---
+++ src/main/java/dev/emergent/populate/Populate.java
@@ -0,0 +1,10 @@
+package dev.emergent.populate;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Populate implements ModInitializer {
+	@Override
+	public void onInitialize() {
+		// TODO
+	}
+}
