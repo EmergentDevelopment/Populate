7ecf58a6afe8125a4cd03d66221b65e52ec25a1e
Guardian <johnsalmans@hotmail.com>
2021-05-17 22:22:47 -0500

    Initial Config Setup
    Added Complete Config dependencies and began initial config setup. Upon further reflection config structure will end up being rather dynamic. as such it would be best designed to be categorized by feature type followed by structure settings.

--- src/main/java/dev/emergent/populate/Populate.java
+++ src/main/java/dev/emergent/populate/Populate.java
@@ -1,10 +1,25 @@
 package dev.emergent.populate;
 
+import me.lortseam.completeconfig.data.Config;
 import net.fabricmc.api.ModInitializer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class Populate implements ModInitializer {
+
+	public static final String MODID = "populate";
+	public static final Logger LOG = LogManager.getLogger(Populate.MODID);
+
 	@Override
 	public void onInitialize() {
 		// TODO
+		
+		LOG.info("Populate Initializing");
+
+		Config config = Config.builder(MODID)
+				.add(new Settings())
+				.build();
+
+		LOG.info("Populate Initialization Complete");
 	}
 }
---
+++ src/main/java/dev/emergent/populate/Settings.java
@@ -0,0 +1,12 @@
+package dev.emergent.populate;
+
+import me.lortseam.completeconfig.api.ConfigContainer;
+import me.lortseam.completeconfig.api.ConfigGroup;
+
+public class Settings implements ConfigContainer {
+
+    public static class SchematicTypes implements ConfigGroup {
+
+    }
+
+}
